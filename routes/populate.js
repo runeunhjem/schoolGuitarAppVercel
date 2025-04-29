2aea8948d80865623c756d7b628480cfc781b71e
runeunhjem <runeunhjem@gmail.com>
2025-04-29 16:36:25 +0200

    Add try catch to populationService and populate

--- routes/populate.js
+++ routes/populate.js
@@ -1,14 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var db = require('../models');
-var PopulateService = require('../services/PopulationService');
-var populateService = new PopulateService(db)
+var db = require("../models");
+var PopulateService = require("../services/PopulationService");
+var populateService = new PopulateService(db);
 
-//Populate DB
-router.post('/', async function (req, res, next) {
+// Populate DB
+router.post("/", async function (req, res, next) {
+  try {
     await populateService.populateDatabase();
     res.status(200).json({ success: true, message: "Database populated successfully." });
-    res.end();
-})
+  } catch (error) {
+    console.error("Error populating database:", error);
+    res.status(500).json({ success: false, message: "Failed to populate database.", error: error.message });
+  }
+});
 
 module.exports = router;
--- services/PopulationService.js
+++ services/PopulationService.js
@@ -6,54 +6,55 @@ const modelData = require("../data/models.json")
 const guitarData = require("../data/guitars.json")
 
 class PopulationService {
-    constructor(db) {
-        this.client = db.sequelize;
-        this.User = db.User;
-        this.Guitar = db.Guitar;
-        this.Color = db.Color;
-        this.Brand = db.Brand;
-        this.Model = db.Model;
-    }
+  constructor(db) {
+    this.client = db.sequelize;
+    this.User = db.User;
+    this.Guitar = db.Guitar;
+    this.Color = db.Color;
+    this.Brand = db.Brand;
+    this.Model = db.Model;
+  }
 
-    async populateDatabase() {
-        try {
-            if (!this.isPopulated) {
-                for (const item of brandData) {
-                    const existingBrands = await this.Brand.findOne({ where: { id: item.Id } });
-                    if (!existingBrands) {
-                        await sequelize.query(item.query);
-                    }
-                }
-                for (const item of colorData) {
-                    const existingColors = await this.Color.findOne({ where: { id: item.Id } });
-                    if (!existingColors) {
-                        await sequelize.query(item.query);
-                    }
-                }
-                for (const item of modelData) {
-                    const existingModels = await this.Model.findOne({ where: { id: item.Id } });
-                    if (!existingModels) {
-                        await sequelize.query(item.query);
-                    }
-                }
-                for (const item of guitarData) {
-                    const existingGuitars = await this.Guitar.findOne({ where: { id: item.Id } });
-                    if (!existingGuitars) {
-                        await sequelize.query(item.query);
-                    }
-                }
-                for (const item of usersData) {
-                    const existingUsers = await this.User.findOne({ where: { id: item.Id } });
-                    if (!existingUsers) {
-                        await sequelize.query(item.query);
-                    }
-                }
-                this.isPopulated = true;
-            }
-        } catch (error) {
-            throw error;
+  async populateDatabase() {
+    try {
+      if (!this.isPopulated) {
+        for (const item of brandData) {
+          const existingBrands = await this.Brand.findOne({ where: { id: item.Id } });
+          if (!existingBrands) {
+            await this.client.query(item.query);
+          }
         }
+        for (const item of colorData) {
+          const existingColors = await this.Color.findOne({ where: { id: item.Id } });
+          if (!existingColors) {
+            await this.client.query(item.query);
+          }
+        }
+        for (const item of modelData) {
+          const existingModels = await this.Model.findOne({ where: { id: item.Id } });
+          if (!existingModels) {
+            await this.client.query(item.query);
+          }
+        }
+        for (const item of guitarData) {
+          const existingGuitars = await this.Guitar.findOne({ where: { id: item.Id } });
+          if (!existingGuitars) {
+            await this.client.query(item.query);
+          }
+        }
+        for (const item of usersData) {
+          const existingUsers = await this.User.findOne({ where: { id: item.Id } });
+          if (!existingUsers) {
+            await this.client.query(item.query);
+          }
+        }
+        this.isPopulated = true;
+      }
+    } catch (error) {
+      console.error("Error populating database in PopulationService:", error);
+      throw error; // Important! Rethrow so route can return 500 properly
     }
+  }
 }
 
-module.exports = PopulationService
+module.exports = PopulationService;
